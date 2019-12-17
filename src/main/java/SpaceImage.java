99c649ee2bfd75409c9f32a047d05852bc15fde5
Diella Agyeman <diellaagyeman@hotmail.co.uk>
2019-12-17 09:26:50 +0000

    advent of code edits

--- src/main/java/SpaceImage.java
+++ src/main/java/SpaceImage.java
@@ -1,8 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -12,26 +10,16 @@ public class SpaceImage {
     private static int width = 25;
     private static int height = 6;
     private static int size = width*height;
-    private static List<Long> numberOfZeros = Arrays.asList();
-    private static List<Integer> onextwo = Arrays.asList();
+    private static List<Long> numberOfZeros = new ArrayList<>();
+    private static List<Long> onextwo = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         SpaceImage si = new SpaceImage();
         String data = readFileAsString("C:\\Users\\diella.agyeman\\Desktop\\input.txt");
+        si.splitDataIntoLayers(data);
 
-
-//        List<Integer> cm = si.convertStringOfPixelsIntoIntegerArray(data);
-//        System.out.println(si.countPixels(cm));
-//        List<List<Integer>> l = si.splitIntoLayer(data);
-//        System.out.println(l);
-
-        for(int i = 0; i < size; i += size) {
-            List<Integer> pixels = si.convertStringOfPixelsIntoIntegerArray(data.substring(i, (i+size) - 1));
-            Map<Integer, Long> countm = si.countPixels(pixels);
-            numberOfZeros.add(countm.get(0));
-            onextwo.add((int) (countm.get(1)*countm.get(2)));
-        }
         System.out.println(numberOfZeros);
+        System.out.println(onextwo);
     }
 
 
@@ -41,36 +29,28 @@ public class SpaceImage {
         return data.trim();
     }
 
-//    public static String splitDataIntoLayers(String pixelData) {
-//        List<String> pixels = Arrays.asList(pixelData.split(""));
-//        String stringPixels = "";
-//        for (String p: pixels) {
-//
-//        }
-//    }
+    public static void splitDataIntoLayers(String data) {
+        for(int i = 0; i < data.length(); i += size) {
+            List<Integer> pixels = convertStringOfPixelsIntoIntegerArray(data.substring(i, (i+size) - 1));
+            Map<Integer, Long> countm = countPixels(pixels);
+            numberOfZeros.add(countm.get(0));
+            onextwo.add(countm.get(1)*countm.get(2));
+        }
+    }
 
-
-    public List<Integer> convertStringOfPixelsIntoIntegerArray(String pixels) {
+    public static List<Integer> convertStringOfPixelsIntoIntegerArray(String pixels) {
         List<String> stringNumArray = Arrays.asList(pixels.split(""));
         return stringNumArray.stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
 
-//    public List<Integer> splitIntoLayer(List<String> pixelData) {
-////        List<List<Integer>> layers = Arrays.asList();
-//        List<Integer> layers = Arrays.asList();
-//        for (int pixel = 0; pixel < pixelData.length(); pixel += size) {
-////        for (int pixel = 0; pixel < 26; pixel += size) {
-//            List<Integer> layer = pixelData.subList(pixel, pixel + size);
-//            layers.add(layer);
-//            numberOfZeros.add(Math.toIntExact(countPixels(layer).get(0)));
-//        }
-////        return layers;
-//    }
-
-    public Map<Integer, Long> countPixels(List<Integer> pixelArray) {
+    public static Map<Integer, Long> countPixels(List<Integer> pixelArray) {
         return countMap = pixelArray.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    public void findSmallestAmountOfZeros() {
+        numberOfZeros.stream().sorted().findFirst().get();
     }
 }
